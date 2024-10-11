97d0f3de5bd39fa00eaf8b6d2c2286ba8067d055
abubakarafridi <abubakarafridi407@gmail.com>
2024-10-11 21:31:24 +0500

    post operation created and tested with postman

--- server.mjs
+++ server.mjs
@@ -33,9 +33,17 @@ app.get("/api/users", async (req, res) => {
     }
 })
 
-// app.push("/", (req, res) => {
-//     res.send("hii")
-// })
+app.post("/api/users", async (req, res) => {
+    const {name, email} = req.body;
+    try {
+        const newUser = new User({ name, email })
+        await newUser.save();
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(500).json({message: "Error creating user", error: error.message})
+    }
+})
+
 // app.put("/", (req, res) => {
 //     res.send("hii")
 // })
