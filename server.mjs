050a4d8ae656d15b936bf3d93020c01535e0cc79
abubakarafridi <abubakarafridi407@gmail.com>
2024-10-11 21:00:03 +0500

    error handler is added

--- server.mjs
+++ server.mjs
@@ -1,11 +1,33 @@
 import express from "express"
+import cors from "cors"
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors())
+app.use(express.json())
+
+function errorHandler(err, res, req, nex) {
+    res.status(err.status || 500).json({
+        message: err.message || "something went wrong",
+        error: true,
+    });
+}
+
 app.get("/", (req, res) => {
     res.send("hii")
 })
+app.push("/", (req, res) => {
+    res.send("hii")
+})
+app.put("/", (req, res) => {
+    res.send("hii")
+})
+app.delete("/", (req, res) => {
+    res.send("hii")
+})
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log("The port is listening on port 3000");
